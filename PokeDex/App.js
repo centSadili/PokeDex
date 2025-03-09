ea90f26e226ea9ba034b70313a44a22490ed9939
centSadili <sadiliva@students.nu-moa.edu.ph>
2025-03-09 23:36:18 +0800

    Build APK debug and release

--- PokeDex/App.js
+++ PokeDex/App.js
@@ -1,17 +1,20 @@
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
 import DisplayPokemons from './components/PokemonComponents/DisplayPokemons';
 import PokemonDetails from './components/PokemonComponents/PokemonDetails';
 
 export default function App() {
-  const nav = createNativeStackNavigator()
+  const Nav = createNativeStackNavigator()
   return (
     <NavigationContainer>
-      <nav.Navigator initialRouteName='PokeDex'>
-        <nav.Screen name='PokeDex' component={DisplayPokemons}/>
-        <nav.Screen name='Pokemon' component={PokemonDetails}/>
-      </nav.Navigator>
+      <Nav.Navigator initialRouteName='PokeDex'>
+        <Nav.Screen name='PokeDex' component={DisplayPokemons}/>
+        <Nav.Screen name='Pokemon' component={PokemonDetails}/>
+      </Nav.Navigator>
     </NavigationContainer>
   );
 }
